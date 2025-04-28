55a8a2169572f6efe0b58cbdbb762b1a5dc50da3
kimia <doulabikimia@gmail.com>
2025-04-28 09:26:11 -0400

    Add files via upload

---
+++ Programming1-finalproject-master/Athlete.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Athlete here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Athlete
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Athlete
+     */
+    public Athlete()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
