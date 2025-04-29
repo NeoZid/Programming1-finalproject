85a85f121c9340a75b759ba8b3f3e52cf5bb45ab
NeoZid <zidereckn06@gmail.com>
2025-04-28 21:14:09 -0400

    added athlete some methods and variables

--- Athlete.java
+++ Athlete.java
@@ -1,33 +1,46 @@
+import java.util.ArrayList;
 
 /**
  * Write a description of class Athlete here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Neo / Kimia
+ * @version 04-29-2025
  */
 public class Athlete
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private String name;
+    private Gender gender;
+    private int age;
+    private double weight;
+    private ArrayList<Activity> activities;
+    private ArrayList<String> athletes;
+    
 
     /**
      * Constructor for objects of class Athlete
      */
-    public Athlete()
+    public Athlete(String name, Gender gender, int age, double weight)
     {
-        // initialise instance variables
-        x = 0;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.weight = weight;
+        this.athletes = new ArrayList<>();
+        this.activities = new ArrayList<>();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public String getName(){
+        return name;
     }
+    
+    public void listAllAthletes(){
+        System.out.println(athletes);
+    }
+    
+    public void activityList()
+    {
+        System.out.println("Regular activies include:");
+    }
+    
+    
 }
