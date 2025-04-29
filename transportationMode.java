772c7bb3e14e3e15fb6498318048fa4d8bf483b2
NeoZid <zidereckn06@gmail.com>
2025-04-28 20:39:46 -0400

    I added transportationMode ENUM and Gender ENUM!

---
+++ transportationMode.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class transportationMode - write a description of the enum class here
+ *
+ * @author Neo / Kimia
+ * @version (version number or date here)
+ */
+public enum transportationMode
+{
+    BIKING, ROLLERSKATES, WALKING, SWIMMING, SKIING
+}
